916eb5e8b62948ff802fe9153fb7ab2aa274ba17
Gustavo S <gustavonnsanches@usp.br>
2024-11-11 14:50:17 -0300

    Criado model do post

---
+++ Blog/fotos/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+from django.conf import settings
+
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=255)
+    descricao = models.CharField(max_length=255)
+    ano_foto = models.IntegerField()
+    data_postagem = models.DateTimeField()
+    url_foto = models.URLField(max_length=200, null=True)
+
+    def __str__(self):
+        return f'{self.titulo} ({self.ano_foto})'
+
+'''
+class Review(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    likes = models.IntegerField(default=0)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'"{self.text}" - {self.author.username}'
+'''
