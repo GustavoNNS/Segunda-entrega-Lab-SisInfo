29130171cd5c54ecc5ca211a1babba4c9f3c2be4
Gustavo S <gustavonnsanches@usp.br>
2024-11-11 21:24:49 -0300

    Página inicial estruturada

--- Blog/fotos/urls.py
+++ Blog/fotos/urls.py
@@ -1,7 +1,23 @@
+
 from django.urls import path
 
 from . import views
 
-app_name = 'fotos'
+#app_name = 'fotos'
+
 urlpatterns = [
+    path('about/', views.about, name='about'),
+    path('', views.index, name='index'),
 ]
+'''
+
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('about/', views.about, name='about'), # adicione esta linha
+    path('', views.index, name='index'),
+]
+'''
--- Blog/fotos/views.py
+++ Blog/fotos/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+
+def index(request):
+    context = {}
+    return render(request, 'fotos/index.html', context)
+
+
+def about(request):
+    context = {}
+    return render(request, 'fotos/about.html', context)
 
 def detail_foto(request):
     pass
 
+def lista_fotos(request):
+    lista = Post.objects.all()
+    context = {'lista' : lista}
+    return render(request, 'fotos/index.html', context)
