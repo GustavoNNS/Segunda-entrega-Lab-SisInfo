c3dd774402b59e241403b4fbd59be3c4cab1060a
Gustavo S <gustavonnsanches@usp.br>
2024-11-17 12:35:23 -0300

    Views funcionais com DjangoForms e Classes genéricas

--- Blog/catalogoFotos/urls.py
+++ Blog/catalogoFotos/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 app_name = 'catalogo'
 urlpatterns = [
-    path('<int:foto_id>/', views.detail_foto, name='detail'),
-    path('', views.catalogo_fotos,name='index'),
+    path('<int:foto_id>/', views.detail_foto_detailView.as_view(), name='detail'),
+    path('', views.catalogo_fotos_listView.as_view(),name='index'),
     path('busca/', views.busca_fotos, name='busca'),
-    path('postar_foto/', views.postar_foto, name='postar'),
-    path('atualizar_post/<int:foto_id>/', views.atualizar_post, name='atualizar'),
-    path('deletar/<int:foto_id>/', views.deletar_foto, name='deletar')
+    path('postar_foto/', views.postar_foto_createView.as_view(), name='postar'),
+    path('atualizar_post/<int:foto_id>/', views.atualizar_post_updateView.as_view(), name='atualizar'),
+    path('deletar/<int:foto_id>/', views.deletar_foto_deleteView.as_view(), name='deletar')
 
 ]
--- Blog/catalogoFotos/views.py
+++ Blog/catalogoFotos/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from .forms import PostForm
+from django.views import generic, View
 
-def catalogo_fotos(request):
-    context = {"lista_fotos": Post.objects.all() }
-    return render(request, 'catalogoFotos/index.html', context)
+class catalogo_fotos_listView(generic.ListView):
+    model = Post
+    template_name = 'catalogoFotos/index.html'
 
-def detail_foto(request, foto_id):
-    foto = get_object_or_404(Post, pk=foto_id)
-    context = {'foto': foto}
-    return render(request, 'catalogoFotos/detail.html', context)
+class detail_foto_detailView(generic.DetailView):
+    model = Post
+    template_name = 'catalogoFotos/detail.html'
+    def get(self, request, foto_id):
+        foto = get_object_or_404(Post, pk=foto_id)
+        context = {'foto': foto}
+        return render(request, 'catalogoFotos/detail.html', context)
 
 def busca_fotos(request):
     context = {}
@@ -21,8 +25,12 @@ def busca_fotos(request):
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-def postar_foto(request):
-    if request.method == 'POST':
+class postar_foto_createView(View):
+    def get(self, request):
+        form = PostForm()
+        context = {'form' : form}
+        return render(request, 'catalogoFotos/postar.html', context)
+    def post(self, request):
         form = PostForm(request.POST)
         if form.is_valid():
             form_titulo = form.cleaned_data['titulo']
@@ -35,25 +43,10 @@ def postar_foto(request):
                         url_foto = form_url_foto)
             foto.save()
             return HttpResponseRedirect(reverse('catalogo:detail', args=(foto.id, )))
-    else:
-        form = PostForm()
-        context = {'form' : form}
-        return render(request, 'catalogoFotos/postar.html', context)
-    
-def atualizar_post(request, foto_id):
-    foto = get_object_or_404(Post, pk=foto_id)
 
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            foto.titulo = form.cleaned_data['titulo']
-            foto.descricao = form.cleaned_data['descricao']
-            foto.ano_foto = form.cleaned_data['ano_foto']
-            foto.url_foto = form.cleaned_data['url_foto']
-            foto.save()
-            return HttpResponseRedirect(
-                reverse('catalogo:detail', args=(foto.id, )))
-    else:
+class atualizar_post_updateView(View):
+    def get(self, request, foto_id):
+        foto = get_object_or_404(Post, pk=foto_id)
         form = PostForm(
             initial={
                 'titulo': foto.titulo,
@@ -63,12 +56,24 @@ def atualizar_post(request, foto_id):
             })
         context = {'foto': foto, 'form':form}
         return render(request, 'catalogoFotos/atualizar.html', context)
-
-def deletar_foto(request, foto_id):
-    foto = get_object_or_404(Post, pk=foto_id)
-
-    if request.method == "POST":
+    def post(self,request, foto_id):
+        foto = get_object_or_404(Post, pk=foto_id)
+        form = PostForm(request.POST)
+        if form.is_valid():
+            foto.titulo = form.cleaned_data['titulo']
+            foto.descricao = form.cleaned_data['descricao']
+            foto.ano_foto = form.cleaned_data['ano_foto']
+            foto.url_foto = form.cleaned_data['url_foto']
+            foto.save()
+            return HttpResponseRedirect(
+                reverse('catalogo:detail', args=(foto.id, )))
+        
+class deletar_foto_deleteView(View):
+    def get(self,request, foto_id):
+        foto = get_object_or_404(Post, pk=foto_id)
+        context = {'foto': foto}
+        return render(request, 'catalogoFotos/deletar.html', context)
+    def post(self,request,foto_id):
+        foto = get_object_or_404(Post, pk=foto_id)
         foto.delete()
         return HttpResponseRedirect(reverse('catalogo:index'))
-    context = {'foto': foto}
-    return render(request, 'catalogoFotos/deletar.html', context)
